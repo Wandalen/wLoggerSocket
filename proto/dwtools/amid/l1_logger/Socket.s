d2ccd49dbbd613a40eaaa6e1b87f2fa96e2cf0af
wandalen <wandalen.me@gmail.com>
2020-06-09 21:13:18 +0300

    .

--- proto/dwtools/amid/l1_logger/Socket.s
+++ proto/dwtools/amid/l1_logger/Socket.s
@@ -20,7 +20,8 @@ if( typeof module !== 'undefined' )
 let _global = _global_;
 let _ = _global_.wTools;
 let Parent = _.PrinterTop;
-let Self = function wLoggerSocketReceiver( o )
+let Self = wLoggerSocketReceiver;
+function wLoggerSocketReceiver( o )
 {
   return _.workpiece.construct( Self, this, arguments );
 }
